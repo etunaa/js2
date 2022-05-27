f1f08d2c0b1361b22811223943d4e0c0d1dd77e8
etunaa <104025100+etunaa@users.noreply.github.com>
2022-05-27 19:32:53 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-const cars=["bmw","volvo","saab","ford","fiat","audi"];
-let text="";
-for (let i = 0; i < cars.length; i++ ){
-text+=cars[i] + "<br>";
-}
-document.getElementById("demo").innerHTML=text;
+// const cars=["bmw","volvo","saab","ford","fiat","audi"];
+// let text="";
+// for (let i = 0; i < cars.length; i++ ){
+// text+=cars[i] + "<br>";
+// }
+// document.getElementById("demo").innerHTML=text;
+
+
+// const arr=[ 15,53,22,198,10,28,16,70,33,951 ];
+// const odds = arr.filter( number => {
+//     return number % 2!==0;
+// });
+// console.log(odds);
+
+
+// function display(x,func){
+//     var message=func(x);
+//     document.write(message);
+// }
+// function welcomeMessage(salary){
+//     if(salary<500)
+//     return "დაბალი ხელფასი";
+//     else if(salary<=1000)
+//     return "საშუალო ხელფასი";
+//     else(salary>1000)
+//     return "მაღალი ხელფასი"
+// }
+
+// display(1500,welcomeMessage)
